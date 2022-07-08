b25a81dd70606610ecc496666bab6438c6cdf5f6
wangbai <wangbai@chengle.cn>
2022-07-08 14:10:55 +0800

    finish sublease test cases

--- test/meta_profile_sublease.js
+++ test/meta_profile_sublease.js
@@ -94,8 +94,39 @@ contract("MetaProfile", function (accounts) {
     const account1 = accounts[1];
     const account2 = accounts[2];
     const account3 = accounts[3];
-    const account4 = accounts[4];
+    const exchange = accounts[4];
 
-    // Sublease the NFT
+    // Approved and lease the NFT
+    await mp.mint(true, {from: account1});
+    let token_id = 4;
+    let expires = timeHelper.getTimestampInSeconds() + 3600;
+    await mp.setApprovalForAll(exchange, true, {from: account1});
+    await mp.lease(token_id, account2, expires, {from: exchange});
+
+    // Withdraw and Fail to lease the NFT
+    await mp.setApprovalForAll(exchange, false, {from: account1});
+    await throwCatch.expectRevert(
+      mp.lease(token_id, account3, expires, {from: exchange})
+    )
+
+    // Approved and sublease the NFT
+    await mp.setApprovalForAll(exchange, true, {from: account1});
+    await mp.sublease(token_id, account2, account3, {from: exchange});
+
+    // Withdraw and Fail to sublease the NFT
+    await mp.setApprovalForAll(exchange, false, {from: account1});
+    await throwCatch.expectRevert(
+      mp.sublease(token_id, account3, account2, {from: exchange})
+    );
+
+    expectExpires = (await mp.leaseExpiresOf(token_id, account3)).toNumber()
+    assert.equal(expectExpires, expires, "the token:"+token_id+" lease of " + account2 + " should be transfered to "+account3);
+    expectExpires = (await mp.leaseExpiresOf(token_id, account2)).toNumber()
+    assert.equal(expectExpires, 0, "the token:"+token_id+" lease of " + account2 + " should be removed");
+
+    // Check the state
+    expectExpires = (await mp.leaseExpiresOf(token_id)).toNumber()
+    assert.equal(expectExpires, expires, "the token:"+token_id+" should have a ongoing lease at least");
+    assert.equal(expectExpires > timeHelper.getTimestampInSeconds(), true, "the token:"+token_id+" should have a ongoing lease at least");
   });
 });
