53119f0897291a71847eddd8a32cf1fa001e697b
wangbai <wangbai@chengle.cn>
2022-07-14 11:52:20 +0800

    delete dependent lib

--- contracts/MetaProfile.sol
+++ contracts/MetaProfile.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
