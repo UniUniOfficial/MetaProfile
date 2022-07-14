95adefaeded7c4cfc8f4f26e3a27a7c13c9f97d5
wangbai <wangbai@chengle.cn>
2022-07-14 12:55:40 +0800

    Update test cases

--- test/meta_profile_mint.js
+++ test/meta_profile_mint.js
@@ -16,25 +16,19 @@ contract("MetaProfile", function (accounts) {
     const account3 = accounts[3];
 
     // Try to mint a NFT to account1
-    await mp.mint(true, {from: account1});
+    await mp.mint({from: account1});
     const account1_nft_num = (await mp.balanceOf(account1)).toNumber();
     assert.equal(account1_nft_num, 1, "It doesn't mint 1 NFT of "+account1);
-    let token_id = 1;
-    assert.equal(await mp.isAllowedForSublease(token_id), true, "token:"+token_id+" should be set true");
   
     // Try to mint 2 NFTs to account2
-    await mp.mint(false, {from: account2});
+    await mp.mint({from: account2});
     const account2_nft_num = (await mp.balanceOf(account2)).toNumber();
     assert.equal(account2_nft_num, 1, "It doesn't mint 1 NFT of "+account2);
-    token_id = 2;
-    assert.equal(await mp.isAllowedForSublease(token_id), false, "token:"+token_id+" should be set false");
 
     // Try to mint a NFT to account3
-    await mp.mint(true, {from: account3});
+    await mp.mint({from: account3});
     const account3_nft_num = (await mp.balanceOf(account3)).toNumber();
     assert.equal(account3_nft_num, 1, "It doesn't mint 1 NFT of "+account3);
-    token_id = 3;
-    assert.equal(await mp.isAllowedForSublease(token_id), true, "token:"+token_id+" should be set true");
 
     // Check the contract state
     const totalSupply = (await mp.totalSupply()).toNumber();
@@ -57,9 +51,6 @@ contract("MetaProfile", function (accounts) {
     await mp.burn(token_id, {from: account1});
     const account1_nft_num = (await mp.balanceOf(account1)).toNumber();
     assert.equal(account1_nft_num, 0, "It should have 0 NFT of "+account1);
-    await throwCatch.expectRevert ( 
-      mp.isAllowedForSublease(token_id)
-    )
 
     // Try to burn the NFT of account2
     token_id = 2;
