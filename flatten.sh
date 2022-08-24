dacbfc6dd680a6ef7ebc5b83a2ef140df8e5b80f
wangbai <wangbai@chengle.cn>
2022-08-24 10:26:18 +0800

    update flatten script

--- flatten.sh
+++ flatten.sh
@@ -1,3 +1,3 @@
 LICENSE_INFO="SPDX-License-Identifier"
 
-./node_modules/.bin/truffle-flattener contracts/MetaProfile.sol | grep -v "$LICENSE_INFO" > flattened/MetaProfile.sol
+./node_modules/.bin/truffle-flattener contracts/MetaProfile.sol | grep -v "// File" | grep -v "$LICENSE_INFO" > flattened/MetaProfile.sol
